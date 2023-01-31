aeac57c63525853399344201e9835cacf652829e
Ray Orolé <orole.ray@protonmail.com>
2023-01-31 18:05:06 +0100

    Change main

--- main.c
+++ main.c
@@ -1,17 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <windows.h>
-#include <Shlobj.h>
-#include <tchar.h>
-
-SHFOLDERAPI SHGetFolderPathA(HWND hwnd, int csidl, HANDLE hToken, DWORD dwFlags, __out_ecount(MAX_PATH) LPSTR pszPath);
-SHFOLDERAPI SHGetFolderPathW(HWND hwnd, int csidl, HANDLE hToken, DWORD dwFlags, __out_ecount(MAX_PATH) LPWSTR pszPath);
-#ifdef UNICODE
-#define SHGetFolderPath SHGetFolderPathW
-#else
-#define SHGetFolderPath SHGetFolderPathA
-#endif
-
 // Local
 #include "payloads.c"
 
@@ -22,16 +11,6 @@ int main(int argc, char *argv[])
 
     unsigned int threadId;
     HANDLE hThread = (HANDLE)_beginthreadex(NULL, 0, showMsgBox, NULL, 0, &threadId);
-
-    char myDocumentsPath[MAX_PATH];
-    if (SUCCEEDED(SHGetSpecialFolderPath(NULL, myDocumentsPath, CSIDL_PERSONAL, FALSE)))
-    {
-        // Use myDocumentsPath
-    }
-    else
-    {
-        // Handle the error
-    }
 
     WaitForSingleObject(hThread, INFINITE);
     CloseHandle(hThread);
